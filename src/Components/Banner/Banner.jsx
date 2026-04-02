4b32972722d3a0a28264c354c2932ea18210ff89
mohsin6862 <mohsinctg2206@gmail.com>
2026-04-03 02:22:16 +0600

    responsive navbar, banner and showcase sections

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,21 +3,21 @@ import bannerImg from '../../assets/products/banner.png'
 import circleImg from '../../assets/products/Rectangle 4.png'
 const Banner = () => {
   return (
-    <div className="my-20 flex gap-16 items-center justify-around">
-      <div className="banner-info space-y-5">
-        <div className="bg-[#E1E7FF] p-2 rounded-4xl max-w-74 flex gap-2 items-center">
+    <div className="my-20 md:flex  items-center justify-around">
+      <div className="banner-info space-y-5 mb-10 md:mb-0">
+        <div className="bg-[#E1E7FF] p-2 rounded-4xl max-w-74 mx-auto lg:mx-0 flex gap-2 items-center">
             <img className="h-2 w-2" src={circleImg} alt="" />
         <p className="text-[rgba(85,54,246,1)] font-medium"> New: AI-Powered Tools Available</p>
         </div>
-        <h1 className="text-[#101727] font-extrabold text-7xl leading-22">
+        <h1 className="text-[#101727] font-extrabold text-5xl lg:text-7xl text-center lg:text-left leading-22">
           Supercharge Your <br /> Digital Workflow
         </h1>
-        <p className="font-light text-[#627382} max-w-2xl">
+        <p className="font-light text-[#627382} max-w-2xl mx-auto lg:mx-0 text-center lg:text-left">
           Access premium AI tools, design assets, templates, and productivity
           software—all in one place. Start creating faster today. Explore
           Products
         </p>
-        <div className="space-x-4 ">
+        <div className="space-x-4 text-center lg:text-left">
             <button className="btn btn-primary rounded-3xl">Explore Products</button>
             <button className="btn btn-outline btn-info rounded-3xl">Watch Demo</button>
         </div>
--- src/Components/Showcase/Showcase.jsx
+++ src/Components/Showcase/Showcase.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 const Showcase = () => {
     return (
-        <div className='bg-gradient-to-r from-[rgba(79,57,246,1)] to-[rgba(149,20,250,1)] p-20 text-white grid grid-cols-3 text-center'>
-            <div className='space-y-2.5 text-center'>
+        <div className='bg-gradient-to-r from-[rgba(79,57,246,1)] to-[rgba(149,20,250,1)] p-20 text-white grid grid-cols-1 md:grid-cols-3 text-center relative'>
+
+            
+            <div className="hidden md:block absolute left-1/3 top-1/2 -translate-y-1/2 h-[40%] w-[2px] bg-white/40"></div>
+
+          
+            <div className="hidden md:block absolute left-2/3 top-1/2 -translate-y-1/2 h-[40%] w-[2px] bg-white/40"></div>
+
+            <div className='space-y-2.5'>
                 <h1 className='text-6xl font-extrabold'>50K+</h1>
                 <p className='font-medium'>Active Users</p>
             </div>
-            <div className='space-y-2.5 text-center'>
+
+            <div className='space-y-2.5'>
                 <h1 className='text-6xl font-extrabold'>200+</h1>
                 <p className='font-medium'>Premium Tools</p>
             </div>
-            <div className='space-y-2.5 text-center'>
+
+            <div className='space-y-2.5'>
                 <h1 className='text-6xl font-extrabold'>4.9+</h1>
                 <p className='font-medium'>Ratings</p>
             </div>
